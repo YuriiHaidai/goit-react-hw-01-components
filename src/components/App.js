7148654aee6ac7fdad8328c2504c07dbd102be34
Gaiday_u <ura.kiev@gmail.com>
2020-05-22 21:20:10 +0300

    fix PropTypes

--- src/components/App.js
+++ src/components/App.js
@@ -8,21 +8,19 @@ import statisticalData from './statistics/statistical-data.json';
 import friends from './friendList/friends.json';
 import transactions from './transactionHistory/transactions.json';
 
-const App = () => {
-  return (
-    <>
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={statisticalData} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={statisticalData} />
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
+  </>
+);
 
 export default App;
